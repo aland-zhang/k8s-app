595f110c569f3e05669b3e504ed1f9074edfa8e1
phadmin <phadmin@gdphdc.com>
2018-12-05 08:29:17 +0800

    增加监控explorter

--- prometheus-helm/install.sh
+++
@@ -1,6 +0,0 @@
-helm install --namespace monitor --name prometheus stable/prometheus \
-  --set alertmanager.persistentVolume.storageClass="nfs-storage" \
-  --set server.persistentVolume.storageClass="nfs-storage"
-
-helm install  --namespace monitor --name grafana stable/grafana \
-  --set persistence.storageClassName="nfs-storage"
